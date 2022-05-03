057bd86405860a32b5b48c7ae610ff700ac911e5
Essa Shomali <eshomali@Essas-MacBook-Pro.local>
2022-05-02 21:57:54 -0400

    added yarg package and implemented basic commands

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,9 @@
 import validator from 'validator';
+
 import chalk from 'chalk';
+
+import yargs from 'yargs';
+import {hideBin} from 'yargs/helpers'; //hideBin is a shorthand for process.argv.slice(2)
 
 /*
 //FileSystem module
@@ -24,8 +28,62 @@ const getNotes = require('./notes');
 //getNotes();
 */
 
-console.log(validator.isEmail('eshomali@gmail.com'));
-console.log(chalk.blue('hello mate'));
+//console.log(validator.isEmail('eshomali@gmail.com'));
+//console.log(chalk.blue('hello mate'));
+//console.log(process.argv);
+
+
+// add command
+yargs(hideBin(process.argv)).command({
+    command: 'add',
+    describe: 'add a new note',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,     //forces Title option to be required (ex: --title="")
+            type: 'string'          //ensures that Title option is a string
+        },
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+}).parse();
+
+// remove command
+yargs(hideBin(process.argv)).command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function () {
+        console.log('removing a note..');
+    }
+}).parse();
+
+// list command
+yargs(hideBin(process.argv)).command({
+    command: 'list',
+    describe: 'list all notes',
+    handler: function () {
+        console.log('listing all notes..');
+    }
+}).parse();
+
+// read command
+yargs(hideBin(process.argv)).command({
+    command: 'read',
+    describe: 'read note',
+    handler: function () {
+        console.log('reading note..');
+    }
+}).parse();
+
+//console.log(yargs(process.argv).argv);
+
 
 
 
